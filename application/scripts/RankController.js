7b19ae272987e6585214b9992cee08818e9b0cad
Josiel Moreira <josiel1nico@gmail.com>
2014-05-29 00:01:52 -0300

    Alteação no RankController

--- application/scripts/RankController.js
+++ application/scripts/RankController.js
@@ -1,11 +1,12 @@
-angular.module("istim").controller("RankController", ['$scope', '$http', function(scope, http){
+angular.module("istim").controller("RankController", ['$scope', '$http', function(scope, http) {
 
-	scope.ranks = [];
+
+	scope.rank_list = [];
 	scope.api_url = "http://istim_ranking.nodejitsu.com";
 
 	http.get(scope.api_url+"/rank")
 		.success(function(data) {
-			scope.ranks = data;
+			scope.rank_list = data;
 			console.log(data);
 		})
 		.error(function(data) {
@@ -13,3 +14,4 @@ angular.module("istim").controller("RankController", ['$scope', '$http', functio
 		});
 
 }]);
+
